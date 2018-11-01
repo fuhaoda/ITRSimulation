ebd98713b544a24a8e9fc8ac68e80b22acfa688b
jieeshen-lilly <jshen@lilly.com>
2018-11-01 10:33:19 -0400

    Separate x_func from the strategy pattern.

--- ITRSimEng_py/case1.py
+++ ITRSimEng_py/case1.py
@@ -6,17 +6,42 @@ from ITRSimpy import *
 # The current a_func is a linear model of X and the y_func is a linear model of X and A
 # User can define customized a_func and y_func
 
+def x_func(sample_size, dg):
+
+    # User need to define the dimension (dim) and boundary (low and high) of the random generated numbers
+    # User can also change the title of the X matrix
+    cont_array = dg.generate('cont', sample_size, dim=4, low=0, high=1)
+    cont_title = [f"X_Cont{i}" for i in range(cont_array.shape[1])]
+    ord_array = dg.generate('ord', sample_size, dim=2, low=0, high=1)
+    ord_title = [f"X_Cont{i}" for i in range(ord_array.shape[1])]
+    nom_array = dg.generate('nom', sample_size, dim=1, low=0, high=1)
+    nom_title = [f"X_Cont{i}" for i in range(nom_array.shape[1])]
+
+    x_array = np.column_stack([cont_array, ord_array, nom_array])
+    x_title = cont_title + ord_title + nom_title
+
+    """
+    # Or user can import a X matrix from the csv file:
+    x_df = pd.read_csv("filename")
+    x_array = np.asarray(x_df)
+    x_title = list(x_df)
+    """
+
+    return x_title, x_array
+
 
 def a_func(x, n_resp):
     """
     Users define the function of A here. The example below uses a linear function:
-        A = -2.5 + 3*X0 + 0*X1 + 1*X2 + 1*X3 + 0*X4 + 0*X5
+        P(A) = sigmoid( -2.5 + 3*X0 + 0*X1 + 1*X2 + 1*X3 + 0*X4 + 0*X5)
 
     :param x: the input X matrix for the a_function
     :param n_resp: the number of possible responses, i.e. treatment options
     :return: a n x 1 matrix of A
     """
-    beta_a = [-2.5, 3, 0, 1, 1, 0, 0]
+
+    beta_a = [-2.5, 3, 0, 1, 1, 0, 0, 0]
+    assert len(beta_a) == x.shape[1]
     z = np.matmul(x, np.array(beta_a).reshape(-1, 1))
     p = 1 / (1 + np.exp(-z))
     a = np.random.binomial(n_resp - 1, p) + 1
@@ -53,13 +78,11 @@ def main():
     """
 
     g = DataGenerator(seed=1)
-    s = SimulationEngine(a_func=a_func,
+    s = SimulationEngine(x_func=x_func,
+                         a_func=a_func,
                          y_func=y_func,
-                         training_size=500,
-                         testing_size=50000,
-                         n_cont=4,
-                         n_ord=1,
-                         n_nom=1,
+                         training_size=5,
+                         testing_size=50,
                          n_resp=2,
                          ydim=2,
                          generator=g)
